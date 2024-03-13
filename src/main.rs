32427c9b62d10634ffdff8bd699448e59e70bcf0
Armitage, Lake <lake.armitage@sky.uk>
2024-03-13 20:46:31 +0000

    feat: print progress and timing to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::fs::File;
 use std::io::Write;
+use std::time::SystemTime;
 
 fn main() {
+    let start = SystemTime::now();
+
     let mut image = File::create("image.ppm").unwrap();
 
     // image
@@ -13,6 +16,7 @@ fn main() {
     writeln!(&mut image, "P3\n{image_width} {image_height}\n255").unwrap();
 
     for j in 0..image_height {
+        print!("\rScanlines remaining: {}  ", image_height - j);
         for i in 0..image_width {
             let r: f64 = (i as f64) / (image_width as f64 - 1.0);
             let g: f64 = (j as f64) / (image_width as f64 - 1.0);
@@ -25,4 +29,6 @@ fn main() {
             writeln!(&mut image, "{r} {g} {b}").unwrap();
         }
     }
+    println!("\rDone!                    ");
+    println!("took {:?}", start.elapsed().expect("unknown"));
 }
